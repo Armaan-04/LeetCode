4612fb1c80bbac96dcd0825ea1262ce5e25586e2
Armaan-04 <alamarmaan2004@gmail.com>
2025-10-06 09:03:00 +0530

    PalindromeNumber

---
+++ Easy/PalindromeNumber.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, x):
+        
+        if (x<0):
+            return False
+        i = x
+        s = 0
+        while (i>0):
+            d = i % 10
+            s = s * 10 + d
+            i = i // 10
+        if(x == s):
+            return True
+        else:
+            return False 
+        
+sol = Solution()
+is_palindrome = print(sol.isPalindrome(12121))
